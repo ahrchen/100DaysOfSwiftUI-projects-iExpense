3f2f8dbd5d843a9063c520003d5a1c62bdbbc57f
Raymond Chen <ahrchen@gmail.com>
2022-02-20 21:56:51 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Raymond Chen on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
